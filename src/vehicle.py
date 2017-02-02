baac290bb13c0202a278bf283d70f39d1ee7b03b
Mark-YH <mark50404@gmail.com>
2017-02-02 22:26:54 +0800

    test

---
+++ src/vehicle.py
@@ -0,0 +1,50 @@
+'''
+Created on 2017/2/2
+
+@author: Mark Hsu
+'''
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+ultrasound_Forward_TRIG = 11
+ultrasound_Forward_ECHO = 12
+ultrasound_Left_TRIG = 13
+ultrasound_Left_ECHO = 15
+ultrasound_Right_TRIG = 16
+ultrasound_Right_ECHO = 18
+
+GPIO.setup(ultrasound_Forward_TRIG, GPIO.OUT)
+GPIO.setup(ultrasound_Forward_ECHO, GPIO.IN)
+GPIO.setup(ultrasound_Left_TRIG, GPIO.OUT)
+GPIO.setup(ultrasound_Left_ECHO, GPIO.IN)
+GPIO.setup(ultrasound_Right_TRIG, GPIO.OUT)
+GPIO.setup(ultrasound_Right_ECHO, GPIO.IN)
+
+def ultrasound(TRIG,ECHO):
+    while True:
+        GPIO.output(TRIG, 0)
+        time.sleep(0.01)
+    
+        GPIO.output(TRIG, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
+    
+        start = time.time()
+        while GPIO.input(ECHO) == 0:
+            start = time.time()
+        while GPIO.input(ECHO) == 1:
+            stop = time.time()
+        distance = (stop - start) * 34000 / 2
+        print(distance)
+
+def main():
+    try:
+        ultrasound(ultrasound_Forward_TRIG, ultrasound_Forward_ECHO)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+if __name__ == '__main__':
+    main()
+
