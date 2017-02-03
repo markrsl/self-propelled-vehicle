e47b0c774f75bdc5989633cedcf22d1cbb4446d4
Mark-YH <mark50404@gmail.com>
2017-02-03 21:00:30 +0800

    test

---
+++ src/main.py
@@ -0,0 +1,53 @@
+'''
+Created on 2017/2/2
+@author: Mark Hsu
+'''
+import time
+from vehicle import vehicle
+
+def run(flag):
+    dSafeForward = 50
+    dSafeSide = 10
+    try:
+#         for i in range(31000):
+#             right4WD()
+        while flag:
+            dforward = vehicle.ultrasound(vehicle.ultrasound_Forward_TRIG, vehicle.ultrasound_Forward_ECHO)
+            dLeft = vehicle.ultrasound(vehicle.ultrasound_Left_TRIG, vehicle.ultrasound_Left_ECHO)
+            dRight = vehicle.ultrasound(vehicle.ultrasound_Right_TRIG, vehicle.ultrasound_Right_ECHO)
+            print('forward:{} \t left:{} \t right:{}'.format(dforward, dLeft, dRight))
+             
+            if dforward < dSafeForward and dLeft < dSafeSide and dRight < dSafeSide:
+                vehicle.stop()
+                print("Here is not safe!!!")
+            elif dforward < dSafeForward and dLeft < dSafeSide and dRight > dSafeSide:
+                vehicle.stop()
+                time.sleep(0.5)
+                for i in range(31000):
+                    vehicle.right4WD()
+                print("Turn RIGHT!!!")
+            elif dforward < dSafeForward and dLeft > dSafeSide and dRight < dSafeSide:
+                vehicle.stop()
+                time.sleep(0.5)
+                for i in range(31000):
+                    vehicle.left4WD()
+                print("Turn Left!!!")
+            elif dforward < dSafeForward and dLeft > dSafeSide and dRight > dSafeSide:
+                vehicle.stop()
+                time.sleep(0.5)         
+                if dLeft >= dRight:
+                    for i in range(31000):
+                        vehicle.left4WD()
+                    print("Turn Left!!!")
+                elif dLeft < dRight:
+                    for i in range(31000):
+                        vehicle.right4WD()
+                    print("Turn RIGHT!!!")
+            elif dforward > dSafeForward:
+                vehicle.forward()
+                print('Keep moving!')
+    except KeyboardInterrupt:
+        print('Bye')
+    finally:
+        vehicle.stop()
+        vehicle.exit()
--- src/vehicle.py
+++ src/vehicle.py
@@ -1,166 +1,123 @@
 '''
-Created on 2017/2/2
-@author: Mark Hsu
+Created on 2017¦~2¤ë3¤é
+
+@author: Administrator
 '''
-import time
 import RPi.GPIO as GPIO
-from curses.ascii import DLE
-
-# Physical numbering
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-
-# Define GPIO
-ultrasound_Forward_TRIG = 11
-ultrasound_Forward_ECHO = 12
-ultrasound_Left_TRIG = 13
-ultrasound_Left_ECHO = 15
-ultrasound_Right_TRIG = 16
-ultrasound_Right_ECHO = 18
-motor_RightFront_in1 = 31
-motor_RightFront_in2 = 33
-motor_RightRear_in3 = 35
-motor_RightRear_in4 = 37
-motor_LeftFront_in1 = 32
-motor_LeftFront_in2 = 36
-motor_LeftRear_in3 = 38
-motor_LeftRear_in4 = 40
-# setup GPIO
-GPIO.setup(ultrasound_Forward_TRIG, GPIO.OUT)
-GPIO.setup(ultrasound_Forward_ECHO, GPIO.IN)
-GPIO.setup(ultrasound_Left_TRIG, GPIO.OUT)
-GPIO.setup(ultrasound_Left_ECHO, GPIO.IN)
-GPIO.setup(ultrasound_Right_TRIG, GPIO.OUT)
-GPIO.setup(ultrasound_Right_ECHO, GPIO.IN)
-GPIO.setup(motor_RightFront_in1, GPIO.OUT)
-GPIO.setup(motor_RightFront_in2, GPIO.OUT)
-GPIO.setup(motor_RightRear_in3, GPIO.OUT)
-GPIO.setup(motor_RightRear_in4, GPIO.OUT)
-GPIO.setup(motor_LeftFront_in1, GPIO.OUT)
-GPIO.setup(motor_LeftFront_in2, GPIO.OUT)
-GPIO.setup(motor_LeftRear_in3, GPIO.OUT)
-GPIO.setup(motor_LeftRear_in4, GPIO.OUT)
-
-def ultrasound(TRIG, ECHO):
-    GPIO.output(TRIG, 0)
-    time.sleep(0.01)
-    GPIO.output(TRIG, 1)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, 0)
-    start = time.time()
-    while GPIO.input(ECHO) == 0:
-        start = time.time()
-    while GPIO.input(ECHO) == 1:
-        stop = time.time()
-    distance = (stop - start) * 34000 / 2
-    return distance
-
-def forward():
-    GPIO.output(motor_RightFront_in1, False)
-    GPIO.output(motor_RightFront_in2, True)
-    GPIO.output(motor_RightRear_in3, False)
-    GPIO.output(motor_RightRear_in4, True)
-    GPIO.output(motor_LeftFront_in1, True)
-    GPIO.output(motor_LeftFront_in2, False)
-    GPIO.output(motor_LeftRear_in3, True)
-    GPIO.output(motor_LeftRear_in4, False)
-
-def backward():
-    GPIO.output(motor_RightFront_in1, True)
-    GPIO.output(motor_RightFront_in2, False)
-    GPIO.output(motor_RightRear_in3, True)
-    GPIO.output(motor_RightRear_in4, False)
-    GPIO.output(motor_LeftFront_in1, False)
-    GPIO.output(motor_LeftFront_in2, True)
-    GPIO.output(motor_LeftRear_in3, False)
-    GPIO.output(motor_LeftRear_in4, True)
-
-def stop():
-    GPIO.output(motor_RightFront_in1, False)
-    GPIO.output(motor_RightFront_in2, False)
-    GPIO.output(motor_RightRear_in3, False)
-    GPIO.output(motor_RightRear_in4, False)
-    GPIO.output(motor_LeftFront_in1, False)
-    GPIO.output(motor_LeftFront_in2, False)
-    GPIO.output(motor_LeftRear_in3, False)
-    GPIO.output(motor_LeftRear_in4, False)
-
-def left():
-    GPIO.output(motor_RightFront_in1, False)
-    GPIO.output(motor_RightFront_in2, True)
-    GPIO.output(motor_RightRear_in3, False)
-    GPIO.output(motor_RightRear_in4, True)
-
-def right():
-    GPIO.output(motor_LeftFront_in1, True)
-    GPIO.output(motor_LeftFront_in2, False)
-    GPIO.output(motor_LeftRear_in3, True)
-    GPIO.output(motor_LeftRear_in4, False)
-def left4WD():
-    GPIO.output(motor_RightFront_in1, False)
-    GPIO.output(motor_RightFront_in2, True)
-    GPIO.output(motor_RightRear_in3, False)
-    GPIO.output(motor_RightRear_in4, True)
-    GPIO.output(motor_LeftFront_in1, False)
-    GPIO.output(motor_LeftFront_in2, True)
-    GPIO.output(motor_LeftRear_in3, False)
-    GPIO.output(motor_LeftRear_in4, True)
-def right4WD():
-    GPIO.output(motor_RightFront_in1, True)
-    GPIO.output(motor_RightFront_in2, False)
-    GPIO.output(motor_RightRear_in3, True)
-    GPIO.output(motor_RightRear_in4, False)
-    GPIO.output(motor_LeftFront_in1, True)
-    GPIO.output(motor_LeftFront_in2, False)
-    GPIO.output(motor_LeftRear_in3, True)
-    GPIO.output(motor_LeftRear_in4, False)
+import time
+class vehicle():
+    # Physical numbering
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
     
-dSafeForward = 50
-dSafeSide = 10
-def main():
-    try:
-#         for i in range(31000):
-#             right4WD()
-        while True:
-            dforward = ultrasound(ultrasound_Forward_TRIG, ultrasound_Forward_ECHO)
-            dLeft = ultrasound(ultrasound_Left_TRIG, ultrasound_Left_ECHO)
-            dRight = ultrasound(ultrasound_Right_TRIG, ultrasound_Right_ECHO)
-            print('forward:{} \t left:{} \t right:{}'.format(dforward, dLeft, dRight))
-             
-            if dforward < dSafeForward and dLeft < dSafeSide and dRight < dSafeSide:
-                stop()
-                print("Here is not safe!!!")
-            elif dforward < dSafeForward and dLeft < dSafeSide and dRight > dSafeSide:
-                stop()
-                time.sleep(0.5)
-                for i in range(31000):
-                    right4WD()
-                print("Turn RIGHT!!!")
-            elif dforward < dSafeForward and dLeft > dSafeSide and dRight < dSafeSide:
-                stop()
-                time.sleep(0.5)
-                for i in range(31000):
-                    left4WD()
-                print("Turn Left!!!")
-            elif dforward < dSafeForward and dLeft > dSafeSide and dRight > dSafeSide:
-                stop()
-                time.sleep(0.5)         
-                if dLeft >= dRight:
-                    for i in range(31000):
-                        left4WD()
-                    print("Turn Left!!!")
-                elif dLeft < dRight:
-                    for i in range(31000):
-                        right4WD()
-                    print("Turn RIGHT!!!")
-            elif dforward > dSafeForward:
-                forward()
-                print('Keep moving!')
-    except KeyboardInterrupt:
-        print('Bye')
-    finally:
-        stop()
+    # Define GPIO
+    ultrasound_Forward_TRIG = 11
+    ultrasound_Forward_ECHO = 12
+    ultrasound_Left_TRIG = 13
+    ultrasound_Left_ECHO = 15
+    ultrasound_Right_TRIG = 16
+    ultrasound_Right_ECHO = 18
+    motor_RightFront_in1 = 31
+    motor_RightFront_in2 = 33
+    motor_RightRear_in3 = 35
+    motor_RightRear_in4 = 37
+    motor_LeftFront_in1 = 32
+    motor_LeftFront_in2 = 36
+    motor_LeftRear_in3 = 38
+    motor_LeftRear_in4 = 40
+    # setup GPIO
+    GPIO.setup(ultrasound_Forward_TRIG, GPIO.OUT)
+    GPIO.setup(ultrasound_Forward_ECHO, GPIO.IN)
+    GPIO.setup(ultrasound_Left_TRIG, GPIO.OUT)
+    GPIO.setup(ultrasound_Left_ECHO, GPIO.IN)
+    GPIO.setup(ultrasound_Right_TRIG, GPIO.OUT)
+    GPIO.setup(ultrasound_Right_ECHO, GPIO.IN)
+    GPIO.setup(motor_RightFront_in1, GPIO.OUT)
+    GPIO.setup(motor_RightFront_in2, GPIO.OUT)
+    GPIO.setup(motor_RightRear_in3, GPIO.OUT)
+    GPIO.setup(motor_RightRear_in4, GPIO.OUT)
+    GPIO.setup(motor_LeftFront_in1, GPIO.OUT)
+    GPIO.setup(motor_LeftFront_in2, GPIO.OUT)
+    GPIO.setup(motor_LeftRear_in3, GPIO.OUT)
+    GPIO.setup(motor_LeftRear_in4, GPIO.OUT)
+    def __init__(self):
+        pass
+    @staticmethod
+    def ultrasound(TRIG, ECHO):
+        GPIO.output(TRIG, 0)
+        time.sleep(0.01)
+        GPIO.output(TRIG, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
+        start = time.time()
+        while GPIO.input(ECHO) == 0:
+            start = time.time()
+        while GPIO.input(ECHO) == 1:
+            stop = time.time()
+        distance = (stop - start) * 34000 / 2
+        return distance
+    @staticmethod
+    def forward(cls):
+        GPIO.output(cls.motor_RightFront_in1, False)
+        GPIO.output(cls.motor_RightFront_in2, True)
+        GPIO.output(cls.motor_RightRear_in3, False)
+        GPIO.output(cls.motor_RightRear_in4, True)
+        GPIO.output(cls.motor_LeftFront_in1, True)
+        GPIO.output(cls.motor_LeftFront_in2, False)
+        GPIO.output(cls.motor_LeftRear_in3, True)
+        GPIO.output(cls.motor_LeftRear_in4, False)
+    @staticmethod
+    def backward(cls):
+        GPIO.output(cls.motor_RightFront_in1, True)
+        GPIO.output(cls.motor_RightFront_in2, False)
+        GPIO.output(cls.motor_RightRear_in3, True)
+        GPIO.output(cls.motor_RightRear_in4, False)
+        GPIO.output(cls.motor_LeftFront_in1, False)
+        GPIO.output(cls.motor_LeftFront_in2, True)
+        GPIO.output(cls.motor_LeftRear_in3, False)
+        GPIO.output(cls.motor_LeftRear_in4, True)
+    @staticmethod
+    def stop(cls):
+        GPIO.output(cls.motor_RightFront_in1, False)
+        GPIO.output(cls.motor_RightFront_in2, False)
+        GPIO.output(cls.motor_RightRear_in3, False)
+        GPIO.output(cls.motor_RightRear_in4, False)
+        GPIO.output(cls.motor_LeftFront_in1, False)
+        GPIO.output(cls.motor_LeftFront_in2, False)
+        GPIO.output(cls.motor_LeftRear_in3, False)
+        GPIO.output(cls.motor_LeftRear_in4, False)
+    @staticmethod
+    def left(cls):
+        GPIO.output(cls.motor_RightFront_in1, False)
+        GPIO.output(cls.motor_RightFront_in2, True)
+        GPIO.output(cls.motor_RightRear_in3, False)
+        GPIO.output(cls.motor_RightRear_in4, True)
+    @staticmethod
+    def right(cls):
+        GPIO.output(cls.motor_LeftFront_in1, True)
+        GPIO.output(cls.motor_LeftFront_in2, False)
+        GPIO.output(cls.motor_LeftRear_in3, True)
+        GPIO.output(cls.motor_LeftRear_in4, False)
+    @staticmethod
+    def left4WD(cls):
+        GPIO.output(cls.motor_RightFront_in1, False)
+        GPIO.output(cls.motor_RightFront_in2, True)
+        GPIO.output(cls.motor_RightRear_in3, False)
+        GPIO.output(cls.motor_RightRear_in4, True)
+        GPIO.output(cls.motor_LeftFront_in1, False)
+        GPIO.output(cls.motor_LeftFront_in2, True)
+        GPIO.output(cls.motor_LeftRear_in3, False)
+        GPIO.output(cls.motor_LeftRear_in4, True)
+    @staticmethod
+    def right4WD(cls):
+        GPIO.output(cls.motor_RightFront_in1, True)
+        GPIO.output(cls.motor_RightFront_in2, False)
+        GPIO.output(cls.motor_RightRear_in3, True)
+        GPIO.output(cls.motor_RightRear_in4, False)
+        GPIO.output(cls.motor_LeftFront_in1, True)
+        GPIO.output(cls.motor_LeftFront_in2, False)
+        GPIO.output(cls.motor_LeftRear_in3, True)
+        GPIO.output(cls.motor_LeftRear_in4, False)
+    @staticmethod
+    def exit():
         GPIO.cleanup()
-if __name__ == '__main__':
-    main()
-
